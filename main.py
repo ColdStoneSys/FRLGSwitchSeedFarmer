55f3cd7acbde85fab25907544791cc1a924b0bd6
lincoln-lm <73306575+lincoln-lm@users.noreply.github.com>
2026-03-14 22:44:59 -0600

    clean consistent namecase

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ with open("config.json", "r", encoding="utf-8") as f:
     config = json.load(f)
 
 bot = SeedBot(config["IP"])
-APressInitialValue = config["APressInitialValue"]
-APressUpperLimit = config["APressUpperLimit"]
-seedsToCollect = config["seedsToCollect"]
-repeatTimes = config["repeatTimes"]
-outputFileName = config["outputFileName"]
+A_PRESS_INITIAL_VALUE = config["APressInitialValue"]
+A_PRESS_UPPER_LIMIT = config["APressUpperLimit"]
+SEEDS_TO_COLLECT = config["seedsToCollect"]
+REPEAT_TIMES = config["repeatTimes"]
+OUTPUT_FILE_NAME = config["outputFileName"]
 
 
 def signal_handler(_signal, _advances):  # CTRL+C handler
@@ -21,57 +21,57 @@ def signal_handler(_signal, _advances):  # CTRL+C handler
 
 signal.signal(signal.SIGINT, signal_handler)
 
-lowVBlankHeralding = 256
-seedsCounter = 0
-VBlankCounter = 0
-repeatCounter = 0
+LOW_VBLANK_HERALDING = 256
+seeds_counter = 0
+vblank_counter = 0
+repeat_counter = 0
 tic = 0
 toc = 0
 
-APressValue = APressInitialValue
-delay = 16.7427 / 1000 / repeatTimes
-resetTime = time()
-consecutiveFailures = 0
+a_press_value = A_PRESS_INITIAL_VALUE
+delay = 16.7427 / 1000 / REPEAT_TIMES
+reset_time = time()
+consecutive_failures = 0
 
 # Make file with header if file did not already exist
-if not os.path.exists(outputFileName):
-    with open(outputFileName, "w", newline="", encoding="utf-8") as file:
+if not os.path.exists(OUTPUT_FILE_NAME):
+    with open(OUTPUT_FILE_NAME, "w", newline="", encoding="utf-8") as file:
         writer = csv.writer(file)
         writer.writerow(["Seed", "Frame", "Time"])
 
 while (
-    seedsCounter < seedsToCollect
-    and APressValue <= APressUpperLimit
-    and consecutiveFailures < 5
+    seeds_counter < SEEDS_TO_COLLECT
+    and a_press_value <= A_PRESS_UPPER_LIMIT
+    and consecutive_failures < 5
 ):
     try:
-        VBlankCounter = bot.read_vblank_counter()
+        vblank_counter = bot.read_vblank_counter()
     except Exception as e:
         print("Error reading, resetting")
         bot.restart_game()
         bot.pause(1)
-        resetTime = time()
-        consecutiveFailures += 1
+        reset_time = time()
+        consecutive_failures += 1
         continue
 
     # Check if we have not reached our "boot succeeded" detection within 5 seconds, reset if need be
-    if tic == 0 and time() - resetTime > 10:
+    if tic == 0 and time() - reset_time > 10:
         # We failed to properly boot, try again
         print("Failed to boot")
-        consecutiveFailures += 1
+        consecutive_failures += 1
         bot.restart_game()
         bot.pause(1)
-        resetTime = time()
+        reset_time = time()
         continue
 
     # Early boot detection, also serves as a fixed time we can produce input timing estimates with
-    if VBlankCounter == lowVBlankHeralding:
+    if vblank_counter == LOW_VBLANK_HERALDING:
         tic = time()
 
     # Attempt to grab seed
-    if VBlankCounter == APressValue:
-        if repeatCounter > 0:
-            bot.pause(delay * repeatCounter)
+    if vblank_counter == a_press_value:
+        if repeat_counter > 0:
+            bot.pause(delay * repeat_counter)
 
         bot.press("A")
         toc = time()
@@ -89,44 +89,44 @@ while (
         # Seed initialization timed out, reset and try again
         if not ok:
             print("Failed to press A at the cutscene")
-            consecutiveFailures += 1
+            consecutive_failures += 1
             tic = 0
             toc = 0
             bot.restart_game()
             bot.pause(1)
-            resetTime = time()
+            reset_time = time()
             continue
 
         # Collect data
         initialSeed = bot.read_initial_seed()
-        seedsCounter += 1
+        seeds_counter += 1
         print(
-            f"{seedsCounter:04d} - {initialSeed:04X} | {APressValue} ({(toc - tic):.4f})"
+            f"{seeds_counter:04d} - {initialSeed:04X} | {a_press_value} ({(toc - tic):.4f})"
         )
 
-        with open(outputFileName, "a", newline="", encoding="utf-8") as file:
+        with open(OUTPUT_FILE_NAME, "a", newline="", encoding="utf-8") as file:
             writer = csv.writer(file)
-            writer.writerow([f"{initialSeed:04X}", APressValue, toc - tic])
+            writer.writerow([f"{initialSeed:04X}", a_press_value, toc - tic])
 
-        repeatCounter += 1
+        repeat_counter += 1
 
-        if repeatCounter == repeatTimes:
-            repeatCounter = 0
-            APressValue += 1
+        if repeat_counter == REPEAT_TIMES:
+            repeat_counter = 0
+            a_press_value += 1
 
-        consecutiveFailures = 0
+        consecutive_failures = 0
         tic = 0
         toc = 0
         bot.restart_game()
-        resetTime = time()
-    elif VBlankCounter > APressValue and tic != 0:
+        reset_time = time()
+    elif vblank_counter > a_press_value and tic != 0:
         print("Missed frame to press A")
-        consecutiveFailures += 1
+        consecutive_failures += 1
         tic = 0
         toc = 0
         bot.restart_game()
         bot.pause(1)
-        resetTime = time()
+        reset_time = time()
         continue
 
     bot.pause(0.001)
