4f70418b6adb89e3c59eaf0baff9f6b20d3336a7
Real96 <chicco.gaeta@gmail.com>
2026-03-16 19:38:02 +0100

    Fix spaces

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ bot = SeedBot(config["IP"])
 INITIAL_SEED_DELAY = config["A_PRESS_INITIAL_VALUE"]
 SEEDS_TO_COLLECT = config["SEEDS_TO_COLLECT"]
 REPEAT_MODE = config["REPEAT_MODE"]
+
 if REPEAT_MODE == "FIXED":
     REPEAT_TIMES = config["REPEAT_TIMES"]
 elif REPEAT_MODE == "AUTO":
@@ -17,6 +18,7 @@ else:
     raise ValueError(
     f"{REPEAT_MODE} is an invalid value for REPEAT_MODE. Acceptable values are 'AUTO' or 'FIXED'."
     )
+
 OUTPUT_FILE_NAME = config["OUTPUT_FILE_NAME"]
 DEBUG = config["DEBUG"]
 
@@ -24,7 +26,6 @@ DEBUG = config["DEBUG"]
 def signal_handler(_signal, _advances):  # CTRL+C handler
     print("Stop request")
     bot.close()
-
 
 signal.signal(signal.SIGINT, signal_handler)
 
@@ -58,19 +59,20 @@ for i in range(90):
         data_one ^= 1
 
     blink_start_good_values[i] = (data_two << 32) | (data_one << 16) | data_zero
-    
+
 blink_start_good_values = tuple(blink_start_good_values)
 seed_delay = INITIAL_SEED_DELAY + seeds_counter
 current_seeds = []
 reset_time = time()
+
 while seeds_counter < SEEDS_TO_COLLECT and consecutive_failures < 5:
     # Verify the game booted and get a time stamp for an event with fixed-time relative to boot
-    
     loop_counter = 0
     prior_blink_data = 0
     tic = 0
     toc = 0
     bot.pause(1)
+
     try:
         vblank_counter = bot.read_vblank_counter()
         while vblank_counter != LOW_VBLANK_HERALDING:
@@ -95,6 +97,7 @@ while seeds_counter < SEEDS_TO_COLLECT and consecutive_failures < 5:
 
     # Stall until the BlinkPressStart task has been initialized
     bot.pause(23)
+ 
     try:
         while not bot.read_is_blink_start_initialized():
             bot.pause(0.001)
@@ -115,6 +118,7 @@ while seeds_counter < SEEDS_TO_COLLECT and consecutive_failures < 5:
             bot.pause(0.001)
             blink_data = bot.read_blink_start_counter()
             index = loop_counter % 90
+
             # Data matches the next expected value in the sequence
             if blink_data == blink_start_good_values[index]:
                 loop_counter += 1
@@ -128,9 +132,9 @@ while seeds_counter < SEEDS_TO_COLLECT and consecutive_failures < 5:
                 prior_one = (prior_blink_data >> 16) & 0xFFFF
 
                 if prior_one == 1:
-                    base  = 0x1e00010000
+                    base = 0x1e00010000
                 else:
-                    base  = 0x3c00000000
+                    base = 0x3c00000000
 
                 # There are a number of edge cases that would only happen extremely rarely if we read in the middle of the function that we test for
                 test_prior = base | prior_zero
@@ -185,6 +189,7 @@ while seeds_counter < SEEDS_TO_COLLECT and consecutive_failures < 5:
     # Stall until seed is initialized
     bot.pause(3)
     ok = False
+
     try:
         ok = bot.read_is_box_pointer_initialized()
     # TODO: actual exception types
@@ -232,6 +237,7 @@ while seeds_counter < SEEDS_TO_COLLECT and consecutive_failures < 5:
 
     if REPEAT_MODE == "FIXED":
         repeat_counter += 1
+
         if repeat_counter == REPEAT_TIMES:
             repeat_counter = 0
             seed_delay += 1
