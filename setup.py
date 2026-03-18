93c0debbfd1a347bdabdeabc50aa1bfa5bda5666
Real96 <chicco.gaeta@gmail.com>
2026-03-18 23:03:34 +0100

    Add requirements to freeze

--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
 from cx_Freeze import setup, Executable
 
+IMPORT_MAP = {
+        "pyusb": "usb"
+}
+
 def get_requirements():
+    frozen_packages = []
+
     with open("requirements.txt") as f:
         # Filter out comments and empty lines
-        return [line.strip() for line in f if line.strip() and not line.startswith("#")]
+        for line in f:
+            line = line.strip()
+
+            if not line or line.startswith("#"):
+                continue
+
+            module_name = IMPORT_MAP.get(line, line)
+            frozen_packages.append(module_name)
+
+        return frozen_packages
 
 executables = [
     Executable("main.py", target_name="frlg_switch_seed_farmer"),
@@ -15,5 +30,5 @@ setup(
     version="1.0",
     description="",
     executables=executables,
-    options={"build_exe": {"packages": ["usb"], "build_exe": "dist", "include_files": ["config.json"]}},
+    options={"build_exe": {"packages": get_requirements(), "build_exe": "dist", "include_files": ["config.json"]}},
 )
