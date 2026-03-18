921be7b7204f9229dcaf886f583b0ec144b35f0f
c-poole <codemeister4@gmail.com>
2026-03-18 15:17:43 -0500

    try add requirements to freeze

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from cx_Freeze import setup, Executable
 
+def get_requirements():
+    with open("requirements.txt") as f:
+        # Filter out comments and empty lines
+        return [line.strip() for line in f if line.strip() and not line.startswith("#")]
+
 executables = [
     Executable("main.py", target_name="frlg_switch_seed_farmer"),
     Executable("process_seeds.py", target_name="frlg_switch_seed_processor"),
@@ -10,5 +15,5 @@ setup(
     version="1.0",
     description="",
     executables=executables,
-    options={"build_exe": {"build_exe": "dist", "include_files": ["config.json"]}},
+    options={"build_exe": {"packages": get_requirements(), "build_exe": "dist", "include_files": ["config.json"]}},
 )
