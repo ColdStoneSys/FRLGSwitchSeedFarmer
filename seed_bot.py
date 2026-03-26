50919fad303ffec4b6325e24bfe8dee4eb94a813
c-poole <codemeister4@gmail.com>
2026-03-26 14:59:49 -0500

    size 8

--- seed_bot.py
+++ seed_bot.py
@@ -333,7 +333,7 @@ class SeedBotUSB(SeedBot):
     def get_title_id(self):
         self.send_command("getTitleID")
 
-        return int.from_bytes(self._read(size=16), "little")
+        return int.from_bytes(self._read(size=8), "little")
 
     def shutdown(self):
         util.dispose_resources(self.device)
